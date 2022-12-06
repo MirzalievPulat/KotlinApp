ddb2c305aa893d402cdf388a5484bda8c59b44a9
MirzalievPulat <mirzaliyevpolat3105@gmail.com>
2022-12-06 20:25:06 +0500

    button added

--- app/src/main/java/com/example/kotlinapp/MainActivity.kt
+++ app/src/main/java/com/example/kotlinapp/MainActivity.kt
@@ -1,11 +1,22 @@
 package com.example.kotlinapp
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val button = findViewById<Button>(R.id.openSecond)
+        button.setOnClickListener(){
+            openSecondPage()
+        }
+    }
+    fun openSecondPage(){
+        val intent = Intent (this, SecondActivity::class.java)
+        startActivity(intent)
     }
 }
